63f7d94897367a65f70400b1c80cd9a6331aa4e4
Lynn <lynn@lynntheory.com>
2020-05-01 14:51:24 -0400

    fix part 2: lynn is blind boogaloo

--- server.js
+++ server.js
@@ -1,26 +1,4 @@
 //required files
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const http = require('http');
-const Express = require('express');
-const app = Express();
-
-const config = require('./config/config.json');
-const cmds = require('./functions/index.js');
-
-//cup of coffee
-setInterval(function() {
-    http.get(`http://lynntheory-discord-tarot-bot.glitch.me/`);
-}, 280000);
-
-client.on('ready', () => {
-  console.log(`Tarot Bot has started, with ${client.users.size} users in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  const presence = config.presence;
-  setInterval(() => {
-    const random = Math.floor(Math.random() * (presence.length - 1) + 1);
-    client.user.setActivity(presence[random], { type: 'WATCHING' });
-  }, 50000)
-});//required files
 const Express = require('express');
 const app = Express();
 const http = require('http');
