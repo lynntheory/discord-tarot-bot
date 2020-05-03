a517f1724f9f878e5c6d4be023c689055239680f
Lynn <lynn@lynntheory.com>
2020-05-03 14:44:52 -0400

    back to image fix

--- cmds/threecard.js
+++ cmds/threecard.js
@@ -18,7 +18,7 @@ async function threecard (args, message) {
       message.channel.send({embed: {
         title: `Your card for the ${spread[i]} is ${cards[i]}.`,
         description: `Card Meaning: ${meaning}`,
-        image: `${imgPath}`
+        image: imgPath
       }})
     }
   } catch (err) {
