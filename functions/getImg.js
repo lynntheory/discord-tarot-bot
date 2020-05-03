06b0c4adba2a1a6b7e2941b3d7a41b16abf6dbce
Lynn <lynn@lynntheory.com>
2020-05-03 15:08:38 -0400

    error testing in repeat readings

--- functions/getImg.js
+++ functions/getImg.js
@@ -1,10 +1,10 @@
 //imports
 const config = require('../config/config.json');
-const deck = require('../img/rider-waite/riderwaiteimg.json');
+const index = require('../img/rider-waite/riderwaiteimg.json');
 
 module.exports.getImg = function(card){
   try{
-    var imgPath = deck[card];
+    var imgPath = index[card];
     return imgPath;
   } catch (err) {
     console.log(err);
