1e508bafeab27a9bebd0d044dbd45647a7d17e73
Lynn <lynn@lynntheory.com>
2020-05-10 14:37:25 -0400

    removing image element

--- cmds/onecard.js
+++ cmds/onecard.js
@@ -10,13 +10,13 @@ async function onecard (args, message, currentDeck) {
     var pullCount = 1;
     var cards = mechanics.cardPull(hand, pullCount, currentDeck);
     var meaning = book.checkBook(cards[0], currentDeck);
-    var imgPath = image.getImg(cards[0], currentDeck);
+    //var imgPath = image.getImg(cards[0], currentDeck);
     message.channel.send({embed: {
       title: `Your card is ${cards[0]}.`,
-      description: `Card Meaning: ${meaning}`,
-      image: {
-        "url": `${imgPath}`
-      }
+      description: `Card Meaning: ${meaning}`
+      //image: {
+      //  "url": `${imgPath}`
+    //  }
     }})
   } catch (err) {
     console.log(err);
