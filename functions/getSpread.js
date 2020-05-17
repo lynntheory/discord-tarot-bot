afeed88e3aca9a44eb56c9e3857ea13db061032a
Lynn <lynn@lynntheory.com>
2020-05-17 14:50:24 -0400

    deep copying spread details

--- functions/getSpread.js
+++ functions/getSpread.js
@@ -5,6 +5,7 @@ const spreadIndex = require('../spreads/spreads.json');
 module.exports.getSpread = function(message, spread, positions, pullCount){
   try{
     var currentSpread = spreadIndex[spread];
+    var currentSpread = JSON.parse(JSON.stringify(spreadIndex[spread]));
     var positions = currentSpread[positions];
     var pullCount = currentSpread[pullCount];
     return positions, pullCount;
