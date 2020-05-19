bd4665afcb1479056ff9a88c0a03ab03f98b57d7
Lynn Theory <lynn@lynntheory.com>
2020-05-19 02:02:31 -0400

    omitted bracket fix

--- functions/getSpread.js
+++ functions/getSpread.js
@@ -3,9 +3,9 @@ const config = require('../config/config.json');
 const spreadIndex = require('../spreads/spreads.json');
 
 module.exports.getSpread = function(message, spread, positions, pullCount){
-  try
+  try {
     spread = JSON.stringify(spread);
-    var spreads = JSON.parse(JSON.stringify(spreadIndex.index));
+    var spreads = JSON.parse(JSON.stringify(spreadIndex.index);
     if (spreads[spread] != undefined){
       var positions = spreadIndex[spread][2];
       var pullCount = spreadIndex[spread][3];
