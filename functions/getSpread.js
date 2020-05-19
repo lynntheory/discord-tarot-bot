758bcf55ac13caa650b42236d13d0cddb1a679d9
Lynn Theory <lynn@lynntheory.com>
2020-05-19 02:00:46 -0400

    trying to figure out issue with json and variable comparison

--- functions/getSpread.js
+++ functions/getSpread.js
@@ -3,8 +3,9 @@ const config = require('../config/config.json');
 const spreadIndex = require('../spreads/spreads.json');
 
 module.exports.getSpread = function(message, spread, positions, pullCount){
-  try{
-    var spreads = spreadIndex.index;
+  try
+    spread = JSON.stringify(spread);
+    var spreads = JSON.parse(JSON.stringify(spreadIndex.index));
     if (spreads[spread] != undefined){
       var positions = spreadIndex[spread][2];
       var pullCount = spreadIndex[spread][3];
