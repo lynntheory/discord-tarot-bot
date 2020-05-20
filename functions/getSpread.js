f0b3489f08a66fcef8b71ff3a53491dd56e7d07a
Lynn Theory <lynn@lynntheory.com>
2020-05-20 13:23:49 -0400

    one last try for the moment

--- functions/getSpread.js
+++ functions/getSpread.js
@@ -1,13 +1,14 @@
 //imports
 const config = require('../config/config.json');
 const spreadIndex = require('../spreads/spreads.json');
+const spreadIndex = JSON.parse(spreadIndex);
 
 module.exports.getSpread = function(message, spread, positions, pullCount){
   try {
-    var spreads = JSON.parse(spreadIndex[index]);
+    var spreads = spreadIndex[index];
     if (spreads[spread] != undefined){
-      var positions = JSON.parse(spreadIndex[spread][2]);
-      var pullCount = JSON.stringify(spreadIndex[spread][3]);
+      var positions = spreadIndex["spreads"][spread][positions];
+      var pullCount = spreadIndex["spreads"][spread][pullCount];
       return positions, pullCount;
     } else {
       message.reply('Cannot recognize card spread.');
