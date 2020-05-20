8b27f84dc3a21a918510eb4437b4f05b2883cad9
Lynn Theory <lynn@lynntheory.com>
2020-05-20 13:35:10 -0400

    why is this not working like the others

--- functions/getSpread.js
+++ functions/getSpread.js
@@ -1,14 +1,14 @@
 //imports
 const config = require('../config/config.json');
+
+//load spreads
 const spreadIndex = require('../spreads/spreads.json');
-var index = JSON.parse(spreadIndex);
 
 module.exports.getSpread = function(message, spread, positions, pullCount){
   try {
-    var spreads = index["index"];
-    if (spreads[spread] != undefined){
-      var positions = index["spreads"][spread][positions];
-      var pullCount = index["spreads"][spread][pullCount];
+    if (spreadIndex[spread] != undefined){
+      var positions = spreadIndex[spread][positions];
+      var pullCount = spreadIndex[spread][pullCount];
       return positions, pullCount;
     } else {
       message.reply('Cannot recognize card spread.');
