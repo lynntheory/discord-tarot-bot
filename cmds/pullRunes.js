779db83adb265abfc40fdf4f9fb6a8b4327078d6
Lynn <lynn@lynntheory.com>
2020-08-09 13:57:01 -0400

    simple pull runes code added

--- cmds/pullRunes.js
+++ cmds/pullRunes.js
@@ -10,28 +10,28 @@ async function pullRunes (message, spread, currentDeck) {
   try{
     var hand = [];
     var spreadData;
-    spreadData = spreadInfo.getSpread(message, spread, spreadData);
+    spreadData = spreadInfo.getRuneSpread(message, spread, spreadData);
     var positions = spreadData.positions;
-    var pullCount = 8;
-    var cards = mechanics.cardPull(hand, pullCount, currentDeck);
+    var pullCount = spreadData.pullCount;
+    var runes = mechanics.runePull(hand, pullCount, currentDeck);
     if (spread != "one"){
       for (var i = 0; i <= pullCount; i++) {
-        var meaning = book.checkBook(cards[i], currentDeck);
-        var imgPath = image.getImg(cards[i], currentDeck);
+        var meaning = book.checkBook(runes[i], currentDeck);
+        var imgPath = image.getImg(runes[i], currentDeck);
           message.channel.send({embed: {
-            title: `Your card for the ${positions[i]} is ${cards[i]}.`,
-            description: `Card Meaning: ${meaning}`,
+            title: `Your rune for the ${positions[i]} is ${runes[i]}.`,
+            description: `Rune Meaning: ${meaning}`,
             image: {
               "url": `${imgPath}`
             }
           }})
         }}
     else {
-      var meaning = book.checkBook(cards[0], currentDeck);
-      var imgPath = image.getImg(cards[0], currentDeck);
+      var meaning = book.checkBook(runes[0], currentDeck);
+      var imgPath = image.getImg(runes[0], currentDeck);
       message.channel.send({embed: {
-        title: `Your card is ${cards[0]}.`,
-        description: `Card Meaning: ${meaning}`,
+        title: `Your runes is ${cards[0]}.`,
+        description: `Rune Meaning: ${meaning}`,
         image: {
           "url": `${imgPath}`
         }
