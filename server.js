898b65428d5051339639f8de679ab3cf6dd4f71e
Lynn <lynn@lynntheory.com>
2020-11-29 15:15:48 -0500

    adding bot connection to database and table setup

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
-//required files
+//external imports
 const Express = require('express');
 const app = Express();
 const http = require('http');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+//internal imports
 const config = require('./config/config.json');
 const cmds = require('./functions/index.js');
 
+
+//sql imports/setup
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+const Log = sequelize.define('Log', {
+  readingID: { primaryKey: true },
+  date: DateTypes.STRING,
+  spread: DateTypes.STRING,
+  deck: DateTypes.STRING,
+  cards: DateTypes.STRING,
+  name: DateTypes.STRING,
+  notes: DateTypes.STRING
+});
+
+//cup of coffee
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
@@ -17,9 +44,13 @@ setInterval(function() {
 }, 300000);
 
 client.on('ready', () => {
+  //startup message
   console.log(`Discord Tarot Bot has started, with ${client.users.size} users in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  //set activity presence for bot
   const presence = "for messages";
   client.user.setActivity(presence, { type: 'WATCHING' });
+  //initiate Log if not already there
+  Log.sync();
 });
 
 client.on('message', async (message) =>{
