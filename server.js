417ecc2e2ddd3a75a9ff37b126b2eeb4fa38391f
Lynn <lynn@lynntheory.com>
2020-11-29 18:41:53 -0500

    testing removing await

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 try {
-  await sequelize.authenticate();
+  sequelize.authenticate();
   console.log('Connection has been established successfully.');
 } catch (error) {
   console.error('Unable to connect to the database:', error);
