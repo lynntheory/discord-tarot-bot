7a74e38326608e998d57d2c6f77df23778983acc
Lynn <lynn@lynntheory.com>
2020-12-07 14:47:44 -0500

    adding base "show last 10" search

--- cmds/recall.js
+++ cmds/recall.js
@@ -4,20 +4,22 @@ const db = require('../database/database.js');
 
 async function recall (message, args) {
   try{
-    var queryString = "SELECT * FROM logs WHERE ";
+    var queryString = "SELECT * FROM logs";
     let searchParam;
     args.shift();
     switch (args[0]){
       case 'name' || 'Name':
-        queryString = queryString.concat('name = ');
+        queryString = queryString.concat(' WHERE name = ');
         break;
       case 'date' || 'Date':
-        queryString = queryString.concat('date = ');
+        queryString = queryString.concat(' WHERE date = ');
         break;
       case 'note' || 'Note':
-        queryString = queryString.concat('note = ');
+        queryString = queryString.concat(' WHERE note = ');
         break;
       default:
+        queryString = queryString.concat(' LIMIT 10');
+        console.log('No further specification found.');
         break;
     }
     args.shift();
