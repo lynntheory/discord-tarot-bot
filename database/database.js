bbbb57939fedd95ee08f8819e64af6f67311611a
Lynn <lynn@lynntheory.com>
2020-12-07 16:53:11 -0500

    removing raw

--- database/database.js
+++ database/database.js
@@ -72,7 +72,7 @@ module.exports.searchDB = function(queryString){
 }
 
 async function makeQueryResult (queryString) {
-  var results = await sequelize.query(queryString, { type: sequelize.QueryTypes.SELECT, raw: TRUE });
+  var results = await sequelize.query(queryString, { type: sequelize.QueryTypes.SELECT });
   console.log(results);
   let resCount = results.length;
   console.log(results.length);
