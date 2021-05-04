e05031bea80ed73c9e1ac4be0b41087c94edf12e
lynntheory <lynn@lynntheory.com>
2021-05-04 17:24:21 -0400

    small tweak

--- cmds/lookup.js
+++ cmds/lookup.js
@@ -29,7 +29,8 @@ async function lookup (message, args) {
     }
     let parse = args.splice(0, 2);
     let i = 0;
-    for (i; i <= list.length; i++) {
+    let length = list.length;
+    for (i; i < length; i++) {
       parse = utility.texthelp();
       if (parse = null) return parse;
     }
